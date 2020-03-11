8932a009a4a42c1f238f181436e6a51af0a9ce93
GitOffMyCode <genevieve.heald@infinityworks.com>
2020-03-11 11:39:12 +0000

    fixed modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import Modal from './Modal';
 
 class Header extends React.Component {
+
+  state = {
+    isModalOpen: false
+  }
+
+  handleClick = () => {
+    const { isModalOpen } = this.state;
+    this.setState({ isModalOpen: !isModalOpen });
+  }
+
   render() {
-    
+
     return (
       <div id='header'>
-        <FontAwesomeIcon id="add-icon" icon={faPlusCircle} />
+        <FontAwesomeIcon id="add-icon" icon={faPlusCircle} onClick={this.handleClick} />
+        <Modal isOpen={this.state.isModalOpen} onClose={this.handleClick}>
+            <div>I have made a modal!</div>
+          </Modal>
         <h1>planet pledge</h1>
         <h2>saving the world, one plastic bottle at a time</h2>
         <p>Today is Friday 6th March 2020</p>
