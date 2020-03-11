53b545ec107d30293980832524e1e15cb6ad50a4
GitOffMyCode <genevieve.heald@infinityworks.com>
2020-03-11 15:32:56 +0000

    added pledge status endpoint

--- src/App.js
+++ src/App.js
@@ -4,31 +4,10 @@ import './App.css';
 import Header from './components/Header.js';
 import Pledge from './components/Pledge.js';
 import Footer from './components/Footer.js';
- 
+
 class App extends React.Component {
   state = {
     pledges: []
-  }
-
-  addPledge = (newPledgeTitle, newPledgeDetail)  => {
-
-    const newPledge = {
-      "pledge_title": newPledgeTitle,
-      "pledge_detail": newPledgeDetail,
-      "pledge_type": "D",
-      "username": "HelenG"
-    }
-
-    axios.post('http://localhost:3002/pledges', newPledge)
-    .then(res => {
-      newPledge.pledge_id = res.data.pledge_id;
-      const pledgeCopy = this.state.pledges.slice();
-      pledgeCopy.push(newPledge);
-      this.setState({ 
-        pledges: pledgeCopy
-      })
-    })
-    .catch(err => console.log(err)) 
   }
 
   componentDidMount() {
@@ -37,9 +16,43 @@ class App extends React.Component {
         this.setState({ pledges: res.data })
       })
       .catch(err => console.log(err))
+    axios.get('http://localhost:3002/pledges_status')
+      .then(res => {
+        console.log(res.data)
+
+        // create empty results array
+        // loop through array and get pledge_id and date[8][9]
+        // add to obj  ->  {pledge_id: 30, date: 05}
+        // push obj to results array
+
+
+
+        // this.generateDailyStatusArray()
+        // this.setState({ pledges: res.data })
+      })
+      .catch(err => console.log(err))
   }
 
   pledgeCount = () => this.state.pledges.length;
+
+  addPledge = (newPledgeTitle, newPledgeDetail) => {
+    const newPledge = {
+      "pledge_title": newPledgeTitle,
+      "pledge_detail": newPledgeDetail,
+      "pledge_type": "D",
+      "username": "HelenG"
+    }
+    axios.post('http://localhost:3002/pledges', newPledge)
+      .then(res => {
+        newPledge.pledge_id = res.data.pledge_id;
+        const pledgeCopy = this.state.pledges.slice();
+        pledgeCopy.push(newPledge);
+        this.setState({
+          pledges: pledgeCopy
+        })
+      })
+      .catch(err => console.log(err))
+  }
 
   deletePledge = (pledge_id) => {
     axios.delete("http://localhost:3002/pledges/" + pledge_id)
@@ -58,10 +71,10 @@ class App extends React.Component {
 
     return (
       <div className='app'>
-        <Header 
-        pledgeCount={this.pledgeCount()} 
-        showModal={this.toggleModal} 
-        addPledgeFunc={this.addPledge}
+        <Header
+          pledgeCount={this.pledgeCount()}
+          showModal={this.toggleModal}
+          addPledgeFunc={this.addPledge}
         />
         <div id="pledge-container">
           {this.state.pledges.map(pledge => {
--- src/components/Pledge.js
+++ src/components/Pledge.js
@@ -4,6 +4,10 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 class Pledge extends React.Component {
 
+  state = {
+    dailyStatus: [true, false, true, false, false, false, true]
+  }
+
   handleClickDel = () => {
     this.props.deleteFunc(this.props.id);
   }
@@ -14,6 +18,41 @@ class Pledge extends React.Component {
       <div id='pledge'>
         <h3 className='pledge-title'>{this.props.title}</h3>
         <p className='pledge-details'>{this.props.detail}</p>
+        <table><tbody>
+          <tr>
+            <td> Mon </td>
+            <td> Tue </td>
+            <td> Wed </td>
+            <td> Thu </td>
+            <td> Fri </td>
+            <td> Sat </td>
+            <td> Sun </td>
+          </tr>
+          <tr>
+            <td>
+              {this.state.dailyStatus[0] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+            <td>
+              {this.state.dailyStatus[1] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+            <td>
+              {this.state.dailyStatus[2] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+            <td>
+              {this.state.dailyStatus[3] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+            <td>
+              {this.state.dailyStatus[4] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+            <td>
+              {this.state.dailyStatus[5] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+            <td>
+              {this.state.dailyStatus[6] ? <span className="pledgeActive"> </span> : <span className="pledgeInactive">  </span>}
+            </td>
+          </tr>
+          </tbody>
+        </table>
         <FontAwesomeIcon icon={faTrash} id="delete" onClick={this.handleClickDel} />
 
       </div>
